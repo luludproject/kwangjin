0aad2d500cbf01bc396191f8c60bde9794ddd9da
unknown <chlalstn9@gmail.com>
2024-10-10 17:03:28 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://sd.jianhomecare.com",
-  logo: "https://sd.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://gg.jianhomecare.com",
+  logo: "https://gg.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
